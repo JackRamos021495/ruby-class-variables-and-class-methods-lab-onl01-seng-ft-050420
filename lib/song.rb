e5d92cface5295caeb79021f054ddef5e6d6bca4
Jackson Ramos <jackramos021495@gmail.com>
2020-05-11 22:39:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,52 +1,39 @@
 class Song
   attr_accessor :name, :artist, :genre
-
   @@count = 0
-  @@genres = []
   @@artists = []
+  @@genres = []
 
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << genre
     @@artists << artist
+    @@genres << genre
   end
 
   def self.count
     @@count
   end
 
-  def self.genres
-    @@genres.uniq!
+  def self.artists
+    @@artists.uniq
   end
 
-  def self.artists
-    @@artists.uniq!
+  def self.genres
+    @@genres.uniq
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre] #if the genre is already in the hash
-        genre_count[genre] += 1
-      else #if it's a new genre
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre]+= 1}
   end
 
   def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] +=1}
     artist_count
   end
+
 end
